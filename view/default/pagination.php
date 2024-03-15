7b6fba1f58ad25b9f3b5df9de747b840722b68e7
yisar <1533540012@qq.com>
2024-03-15 19:43:44 +0800

    emmm

--- view/default/pagination.php
+++ view/default/pagination.php
@@ -1,15 +1,26 @@
 <?php
 defined('ACC') || exit('ACC Denied');
-$url = $_SERVER['PHP_SELF'];
+$url = $_SERVER['SCRIPT_NAME'];
+$query = explode('&', $_SERVER['QUERY_STRING']);
+foreach ($query as $key => $val) {
+	if (substr($val, 0, 4) === 'page' || $val === '') {
+		unset($query[$key]);
+	}
+}
+$query[] = '';
+$query = implode('&', $query);
+$url = $url . '?' . $query;
 ?>
 <ul class="pagination">
-	<a href="<?php echo $url . '&page='($curr_page - 1) ?>">
+	<a href="<?php echo $url . 'page=' . ($curr_page - 1) ?>">
 		<li>Previous Page</li>
 	</a>
-	<li>
-		<?php echo $curr_page ?>
-	</li>
-	<a href="<?php echo $url . '&page='($curr_page + 1) ?>">
+	<a href="">
+		<li>
+			<?php echo $curr_page ?>
+		</li>
+	</a>
+	<a href="<?php echo $url . 'page=' . ($curr_page + 1) ?>">
 		<li>Next Page</li>
 	</a>
 </ul>
