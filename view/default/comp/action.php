c18321f1cc059f5b342fd5885194b57c4e4ff268
yisar <1533540012@qq.com>
2024-03-15 16:35:24 +0800

    搞定

---
+++ view/default/comp/action.php
@@ -0,0 +1,21 @@
+<div class="action">
+    <?php if (userModel::isLogin()) { ?>
+        <span>[<a target="_blank"
+                href="post.php?id=<?php echo $report_tid; ?>&reply=回复<?php echo $t['tid'] ?>">举报</a>]</span>
+        <span>[<a target="_blank" href="subscribe.php?tid=<?php echo $t['tid'] ?>">
+                <?php echo subscriptionModel::isSubscribed($t['tid']) ? '取消订阅' : '订阅'; ?>
+            </a>]</span>
+    <?php } ?>
+    <?php if (userModel::isLogin() && $_SESSION['type'] > 0) { ?>
+        <span>[<a target="_blank" href="edit.php?tid=<?php echo $t['tid'] ?>">编辑</a>]</span>
+        <span>[<a onclick="if(!confirm('要<?php echo $t['SAGE'] == 1 ? '解除' : ''; ?>SAGE吗?')){return false;};"
+                href="sage.php?tid=<?php echo $t['tid'] ?>">
+                <?php echo $t['SAGE'] == 1 ? '解除' : ''; ?>SAGE
+            </a>]</span>
+        <span>[<a onclick="if(!confirm('确实要删除吗?')){return false;};"
+                href="delete.php?tid=<?php echo $t['tid'] ?>">删除</a>]</span>
+    <?php } ?>
+    <span>[<a target="_blank" href="post.php?id=<?php echo $t['tid']; ?>">回应</a>]</span>
+    <article class="content" content="<?php echo $t['content']; ?>"></article>
+    <?php echo $t['SAGE'] == 0 ? '' : '<p><i class="iconfont icon-cai"></i> 本串已经被SAGE（<abbr  title="该串不会因为新回应而被顶到页首">?</abbr>）</p>' ?>
+</div>
--- view/default/post.php
+++ view/default/post.php
@@ -17,76 +17,56 @@
 <body>
     <?php require(ROOT . 'view/default/header.php'); ?>
     <main>
-            <div class="list">
-                <?php
-
-                foreach ($threads as $k => $t) {
-                    ?>
-                    <div class="item" tid="<?php echo $t['tid']; ?>">
-                        <span>
-                            <?php echo $t['title']; ?>
-                        </span>
-                        <span>
-                            <?php echo $t['name']; ?>
-                        </span>
-                        <span>
-                            <?php echo date('Y-m-d H:i:s', $t['pubtime']); ?>
-                        </span>
-                        <?php $ID = $t['type'] > 0 ? userModel::getUsername($t['uid']) : substr(md5($t['uid'] . $t['title'] . $t['tid']), -8); ?>
-                        <span>ID:
-                            <?php echo $ID; ?>
-                        </span>
-                        <?php if (userModel::isLogin()) { ?>
-                            <span>[<a target="_blank"
-                                    href="view.php?id=<?php echo $report_tid; ?>&reply=%3e%3e<?php echo $t['tid'] ?>">举报</a>]</span>
-                            <span>[<a target="_blank" href="subscribe.php?tid=<?php echo $t['tid'] ?>">
-                                    <?php echo subscriptionModel::isSubscribed($t['tid']) ? '取消订阅' : '订阅'; ?>
-                                </a>]</span>
-                        <?php } ?>
-                        <?php if (userModel::isLogin() && $_SESSION['type'] > 0) { ?>
-                            <span>[<a target="_blank" href="edit.php?tid=<?php echo $t['tid'] ?>">编辑</a>]</span>
-                            <span>[<a
-                                    onclick="if(!confirm('要<?php echo $t['SAGE'] == 1 ? '解除' : ''; ?>SAGE吗?')){return false;};"
-                                    href="sage.php?tid=<?php echo $t['tid'] ?>">
-                                    <?php echo $t['SAGE'] == 1 ? '解除' : ''; ?>SAGE
-                                </a>]</span>
-                            <span>[<a onclick="if(!confirm('确实要删除吗?')){return false;};"
-                                    href="delete.php?tid=<?php echo $t['tid'] ?>">删除</a>]</span>
-                        <?php } ?>
-                        <span>[<a target="_blank" href="view.php?id=<?php echo $t['tid']; ?>">回应</a>]</span>
-                        <article class="content" content="<?php echo $t['content']; ?>"></article>
-                        <?php echo $t['SAGE'] == 0 ? '' : '<p><i class="iconfont icon-cai"></i> 本串已经被SAGE（<abbr  title="该串不会因为新回应而被顶到页首">?</abbr>）</p>' ?>
-                        <?php
-                        $reply_num = $msg->countReplies($t['tid']);
-                        if ($reply_num - 5 > 0) {
-                            echo '<p style="color:#707070"><i class="iconfont icon-jia"></i> 提示: 回应有' . ($reply_num - 5) . '篇被省略。要阅读所有回应请按下回应链接。</p>';
-                        }
-                        ?>
-                    </div>
-                    <?php foreach ($replies[$k] as $key => $value) {
-                        ?>
-                        <div class="item" floor="<?php echo $value['floor'], '>#', $value['floor']; ?>">
-                            <div class="indent">
-                                <span></span>
-                                <span>
-                                    <?php echo $value['name']; ?>
-                                </span>
-                                <span>
-                                    <?php echo date('Y-m-d H:i:s', $value['reptime']); ?>
-                                </span>
-                                <span>ID:
-                                    <?php echo $ID; ?>
-                                </span>
-                                <?php echo $value['uid'] == $t['uid'] ? '<span class="text-po">(PO主)</span>' : ''; ?>
-                                <span>[<a
-                                        href="view.php?id=<?php echo $t['tid']; ?>&reply=%3e%3e<?php echo $t['tid'], '%3e', $value['floor']; ?>#reply">回应</a>]</span>
-                                <article class="content" content="<?php echo $value['content']; ?>"></article>
-                            </div>
+        <style>
+            body {
+                background: radial-gradient(#591803, #fff6df);
+            }
+        </style>
+        <ul class="thread wrap">
+            <?php foreach ($threads as $k => $t) { ?>
+                <div class="item" tid="<?php echo $t['tid']; ?>">
+                    <h1>
+                        <?php echo $t['title']; ?>
+                    </h1>
+                    <div class="info">
+                        <div class="user">
+                            <span>
+                                <?php echo $t['name']; ?>
+                            </span>
+                            <time>
+                                <?php echo date('Y-m-d H:i', $t['pubtime']); ?>
+                            </time>
                         </div>
-                    <?php } ?>
+                        <?php require(ROOT . 'view/default/comp/action.php') ?>
+                    </div>
+
+                </div>
+                <?php foreach ($replies[$k] as $key => $value) {
+                    ?>
+                    <div class="item" floor="<?php echo $value['floor'], '>#', $value['floor']; ?>">
+                        <div class="indent">
+                            <span></span>
+                            <span>
+                                <?php echo $value['name']; ?>
+                            </span>
+                            <span>
+                                <?php echo date('Y-m-d H:i:s', $value['reptime']); ?>
+                            </span>
+                            <span>ID:
+                                <?php echo $ID; ?>
+                            </span>
+                            <?php echo $value['uid'] == $t['uid'] ? '<span class="text-po">(PO主)</span>' : ''; ?>
+                            <span>[<a
+                                    href="view.php?id=<?php echo $t['tid']; ?>&reply=%3e%3e<?php echo $t['tid'], '%3e', $value['floor']; ?>#reply">回应</a>]</span>
+                            <article class="content" content="<?php echo $value['content']; ?>"></article>
+                        </div>
+                    </div>
                 <?php } ?>
+            <?php } ?>
             </div>
-        </div>
+
+        </ul>
+
     </main>
     <?php require(ROOT . 'view/default/comp/editor.php') ?>
     <?php require(ROOT . 'view/default/footer.php') ?>
