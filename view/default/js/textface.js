eaab21f11696f36d9af20a46910031a850e4e33e
yisar <1533540012@qq.com>
2024-03-15 21:16:53 +0800

    emmm

--- view/default/js/textface.js
+++
@@ -1,7 +0,0 @@
-function addtext() {
-	var
-		face = document.querySelector('#textface').value,
-		content = document.querySelector('#content'),
-		text = content.value;
-	content.value = (text + face);
-}
